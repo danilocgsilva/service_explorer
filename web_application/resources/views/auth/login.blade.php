0cccca29563e091d61561500c91e39279d6aeba2
Danilo Silva <danilocgsilva@gmail.com>
2020-01-20 07:16:48 -0300

    csrf to login

--- web_application/resources/views/auth/login.blade.php
+++ web_application/resources/views/auth/login.blade.php
@@ -5,11 +5,23 @@
 <h1>Login</h1>
 
 <form action="" method="post">
-<div class="form-group">
-  <label for="email">E-mail</label>
-  <input type="text"
-    class="form-control" name="email" id="email" aria-describedby="email">
-</div>
+
+    <div class="form-group">
+    <label for="email">E-mail</label>
+    <input type="text"
+        class="form-control" name="email" id="email" aria-describedby="email">
+    </div>
+
+    <div class="form-group">
+    <label for="password">Password</label>
+    <input type="password"
+        class="form-control form-control-sm" name="password" id="password" aria-describedby="helpId" placeholder="">
+    </div>
+
+    @csrf
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+
 </form>
 
 
