c51f8e3a97d46444371cc80d076bd5705860b8fa
Danilo Silva <danilocgsilva@gmail.com>
2020-01-26 16:48:17 -0300

    Starting to show errors in the login form

--- web_application/resources/views/auth/login.blade.php
+++ web_application/resources/views/auth/login.blade.php
@@ -7,15 +7,16 @@
 <form action="{{ route('login') }}" method="post">
 
     <div class="form-group">
-    <label for="email">E-mail</label>
-    <input type="text"
-        class="form-control" name="email" id="email" aria-describedby="email">
+        <label for="email">E-mail</label>
+        <input type="text" class="form-control" name="email" id="email" aria-describedby="email">
+        @if ($errors->has('email'))
+            <small id="emailHelp" class="form-text text-muted">{{ $errors-> }}</small>
+        @endif
     </div>
 
     <div class="form-group">
-    <label for="password">Password</label>
-    <input type="password"
-        class="form-control form-control-sm" name="password" id="password" aria-describedby="helpId" placeholder="">
+        <label for="password">Password</label>
+        <input type="password" class="form-control form-control-sm" name="password" id="password" aria-describedby="helpId" placeholder="">
     </div>
 
     @csrf
