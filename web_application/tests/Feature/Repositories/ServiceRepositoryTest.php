7a7188026f0f5d7dace9749040e386a40bdc08fe
Danilo Silva <danilocgsilva@gmail.com>
2020-01-26 16:28:33 -0300

    New test for service

--- web_application/tests/Feature/Repositories/ServiceRepositoryTest.php
+++ web_application/tests/Feature/Repositories/ServiceRepositoryTest.php
@@ -2,11 +2,12 @@
 
 namespace Tests\Feature\Repositories;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
+use App\Repositories\ServerRepository;
 use App\Repositories\ServiceRepository;
 use App\Service;
+use App\Server;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class ServiceRepositoryTest extends TestCase
 {
@@ -17,10 +18,16 @@ class ServiceRepositoryTest extends TestCase
      */
     private $serviceRepository;
 
+    /**
+     * @var ServerRepository
+     */
+    private $serverRepository;
+
     public function setUp() : void
     {
         parent::setUp();
         $this->serviceRepository = new ServiceRepository();
+        $this->serverRepository = new ServerRepository();
     }
 
     /**
@@ -39,5 +46,25 @@ class ServiceRepositoryTest extends TestCase
         $queryRetrieve = Service::where('name', '=', $name)->where('port', '=', $port)->serverIp($serverIp);
 
         $this->assertTrue($queryRetrieve->exists());
+    }
+
+    /**
+     * If at service creation the not existing server becomes created
+     *
+     * @return void
+     */
+    public function testNewServerCreatedAtNewServiceCreatiion() : void
+    {
+        $serverIp = '188.192.254.0';
+
+        $queryFetchesServer = Server::where(['ip' => $serverIp]);
+
+        if ($queryFetchesServer->exists()) {
+            $queryFetchesServer->delete();
+        }
+
+        $this->serviceRepository->createService('FTP', $serverIp, 22);
+
+        $this->assertTrue($queryFetchesServer->exists());
     }
 }
